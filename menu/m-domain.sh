cad5fc39e174658c06d788e70549056ea5527d50
MyLyy <134211879+LynzVPN@users.noreply.github.com>
2023-06-30 00:27:38 +0800

    Add files via upload

--- menu/m-domain.sh
+++ menu/m-domain.sh
@@ -2,6 +2,63 @@
 MYIP=$(wget -qO- ipv4.icanhazip.com);
 echo "Checking VPS"
 clear
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/LynzVPN/permission/main/ipmini > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS https://raw.githubusercontent.com/LynzVPN/permission/main/ipmini | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/LynzVPN/permission/main/ipmini | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+red='\e[1;31m'
+green='\e[1;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+
+if [ "$res" = "Expired" ]; then
+Exp="\e[36mExpired\033[0m"
+else
+Exp=$(curl -sS https://raw.githubusercontent.com/LynzVPN/permission/main/ipmini | grep $MYIP | awk '{print $3}')
+fi
+clear
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "\E[0;100;33m        • DOMAIN MENU •            \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
--- menu/menu.sh
+++ menu/menu.sh
@@ -2,6 +2,62 @@
 MYIP=$(curl -sS ifconfig.me)
 echo "Checking VPS"
 clear
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/LynzVPN/permission/main/ipmini > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS https://raw.githubusercontent.com/LynzVPN/permission/main/ipmini | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/LynzVPN/permission/main/ipmini | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+red='\e[1;31m'
+green='\e[1;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+
+if [ "$res" = "Expired" ]; then
+Exp="\e[36mExpired\033[0m"
+else
+Exp=$(curl -sS https://raw.githubusercontent.com/LynzVPN/permission/main/ipmini | grep $MYIP | awk '{print $3}')
+fi
 # Color Validation
 DF='\e[39m'
 Bold='\e[1m'
@@ -54,8 +110,8 @@ dmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $3" "substr ($
 umon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $6" "substr ($7, 1, 1)}')"
 tmon="$(vnstat -i eth0 -m | grep "`date +"%b '%y"`" | awk '{print $9" "substr ($10, 1, 1)}')"
 # user
-Exp2=$"Lifetime"
-Name=$"givpn"
+Exp2=$"$exp2"
+Name=$"$user"
 # Getting CPU Information
 cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
 cpu_usage="$((${cpu_usage1/\.*} / ${corediilik:-1}))"
@@ -75,45 +131,42 @@ tram=$( free -m | awk 'NR==2 {print $2}' )
 uram=$( free -m | awk 'NR==2 {print $3}' )
 fram=$( free -m | awk 'NR==2 {print $4}' )
 clear 
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e "\e[1;34m                      VPS INFO                    \e[0m"
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e "\e[1;32m OS            \e[0m: "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`	
-echo -e "\e[1;32m Uptime        \e[0m: $uptime"
-echo -e "\e[1;32m Public IP     \e[0m: $IPVPS"
-echo -e "\e[1;32m Country       \e[0m: $LOC"
+echo -e "\e[0;36m╒════════════════════════════════════════════════╕\033[0m"
+echo -e " \E[44;1;39m                Server Information              \E[0m"
+echo -e "\e[0;36m╘════════════════════════════════════════════════╛\033[0m"
+echo -e "\e[0m OS            \e[0m: "`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`	
+echo -e "\e[0m Uptime        \e[0m: $uptime"
+echo -e "\e[0m Public IP     \e[0m: $IPVPS"
+echo -e "\e[0m Country       \e[0m: $LOC"
 #echo -e "\e[1;32m ASN           \e[0m: $ISP"
 #echo -e "\e[1;32m CITY          \e[0m: $CITY"
-echo -e "\e[1;32m DOMAIN        \e[0m: $domain"	
-echo -e "\e[1;32m DATE & TIME   \e[0m: $DATE2"
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e "\e[1;34m                      RAM INFO                    \e[0m"
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e ""
-echo -e "\e[1;32m RAM USED   \e[0m: $uram MB"	
-echo -e "\e[1;32m RAM TOTAL  \e[0m: $tram MB"
-echo -e ""
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e "\e[1;34m                       MENU                       \e[0m"
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
+echo -e "\e[0m DOMAIN        \e[0m: $domain"	
+echo -e "\e[0m DATE & TIME   \e[0m: $DATE2"
+echo -e "\e[0m RAM USED   \e[0m   : $uram MB"	
+echo -e "\e[0m RAM TOTAL  \e[0m   : $tram MB"
+echo -e "\e[0;36m╒════════════════════════════════════════════════╕\033[0m"
+echo -e "\e[0m\033[0m ${BOLD}\e[0;36mXXX\e[0m SSH \e[0;36m XXX \e[0mVMESS  \e[0;36mXXX  \e[0mVLESS  \e[0;36mXXX  \e[0mTROJAN\e[0;36m XXX $NC "
+echo -e "  ${BIBlue}   \033[0m \e[0m     $ssh1         $vma           $vla           $tra              \e[0m"
+echo -e "\e[0;36m╘════════════════════════════════════════════════╛\033[0m"
+echo -e "\e[0;36m╒════════════════════════════════════════════════╕\033[0m"
+echo -e " \E[44;1;39m                     Menu                       \E[0m"
+echo -e "\e[0;36m╘════════════════════════════════════════════════╛\033[0m"
+echo -e "\e[0;36m〔 1 〕\e[0mMenu SSH"
+echo -e "\e[0;36m〔 2 〕\e[0mMenu Vmess"
+echo -e "\e[0;36m〔 3 〕\e[0mMenu Vless"
+echo -e "\e[0;36m〔 4 〕\e[0mMenu Trojan"
+echo -e "\e[0;36m〔 5 〕\e[0mMenu Shadowsocks"
+echo -e "\e[0;36m〔 6 〕\e[0mMenu Setting"
+echo -e "\e[0;36m〔 7 〕\e[0mStatus Service"
+echo -e "\e[0;36m〔 8 〕\e[0mClear RAM Cache"
+echo -e "\e[0;36m〔 9 〕\e[0mReboot VPS"
 echo -e   ""
-echo -e "\e[1;36m 1 \e[0m: Menu SSH"
-echo -e "\e[1;36m 2 \e[0m: Menu Vmess"
-echo -e "\e[1;36m 3 \e[0m: Menu Vless"
-echo -e "\e[1;36m 4 \e[0m: Menu Trojan"
-echo -e "\e[1;36m 5 \e[0m: Menu Shadowsocks"
-echo -e "\e[1;36m 6 \e[0m: Menu Setting"
-echo -e "\e[1;36m 7 \e[0m: Status Service"
-echo -e "\e[1;36m 8 \e[0m: Clear RAM Cache"
-echo -e "\e[1;36m 9 \e[0m: Reboot VPS"
-echo -e "\e[1;36m x \e[0m: Exit Script"
-echo -e   ""
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e "\e[1;32m Client Name \e[0m: $Name"
-echo -e "\e[1;32m Expired     \e[0m: $Exp2"
-echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e   ""
-echo -e "\e[1;36m --------------------LynzTunnel-------------------\e[0m"
+echo -e "\e[0;36m〔 x 〕\e[0mExit Script"
+echo -e "\e[0;36m╒════════════════════════════════════════════════╕\033[0m"
+echo -e "  \e[0;36mVersion       \e[0m:  V.3 Tunnel"
+echo -e "  \e[0;36mUser       \e[0m   :\033[1;36m $Name \e[0m"
+echo -e "  \e[0;36mExpiry script\e[0m : ${NC}$Exp${NC} "
+echo -e "\e[0;36m╘════════════════════════════════════════════════╛\033[0m"
 echo -e   ""
 read -p " Select menu :  "  opt
 echo -e   ""
